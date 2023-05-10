b95ac5ab6eb26dff2288f014e1df4fe390929194
Tim Dolan <timmydolan14@gmail.com>
2023-05-10 18:17:51 -0400

    Association Updates

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,11 +6,27 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
+
 // Categories have many Products
 
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: 'ProductTag',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: 'ProductTag',
+});
+
 
 module.exports = {
   Product,
